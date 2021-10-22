07e3f6d3529cd4547a1dd0682204529895957312
tonywong586 <72515867+tonywong586@users.noreply.github.com>
2021-10-22 17:09:39 +0800

    square add

--- Monopoly.java
+++ Monopoly.java
@@ -8,9 +8,25 @@ public class Monopoly extends GameData {
 
     Monopoly() {
         squares[0] = new GoSquare("GO", 0);
-        squares[1] = new PropertySquare("Central", 1, 800, 100, EColor.BLUE);
+        squares[1] = new PropertySquare("Central", 1, 800, 90, EColor.BLUE);
         squares[2] = new PropertySquare("Wan Chai", 2, 700, 65, EColor.BLUE);
         squares[3] = new TaxSquare("INCOME TAX",3);
+        squares[4] = new PropertySquare("Stanley", 4, 600, 60, EColor.BLUE);
+        squares[5] = new JailSquare("JailSquare", 5);
+        squares[6] = new PropertySquare("Shek O", 6, 400, 10, EColor.RED);
+        squares[7] = new PropertySquare("Mong Kok", 7, 500, 40, EColor.RED);
+        squares[8] = new ChanceSquare("Chance", 8);
+        squares[9] = new PropertySquare("Tsing Yi", 9, 400, 15, EColor.RED);
+        squares[10] = new Square("FREE PARKING", 10);
+        squares[11] = new PropertySquare("Shatin", 11, 700, 75, EColor.DARK_BLUE);
+        squares[12] = new ChanceSquare("Chance", 12);    
+        squares[13] = new PropertySquare("Tuen Mun", 13, 400, 20, EColor.DARK_BLUE);
+        squares[14] = new PropertySquare("Tai Po", 14, 500, 25, EColor.DARK_BLUE);
+        squares[15] = new GoJailSquare("GO TO JAIL", 15);
+        squares[16] = new PropertySquare("Sai Kung", 16, 400, 10, EColor.YELLOW);
+        squares[17] = new PropertySquare("Yuen Long", 17, 400, 25, EColor.YELLOW);
+        squares[18] = new ChanceSquare("Chance", 18); 
+        squares[19] = new PropertySquare("Tai O", 19, 600, 25, EColor.YELLOW);
     }
 
     // Start the game
