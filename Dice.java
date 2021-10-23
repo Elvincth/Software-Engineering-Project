7e5920836ad430fdd8c4d385f5dfd99792e71b14
Elvin Chu <elvincth@gmail.com>
2021-10-24 01:34:29 +0800

    .

--- Dice.java
+++ Dice.java
@@ -2,6 +2,7 @@ public class Dice implements DiceAPI {
     private int[][] dice = { { 4, 2, 3 }, { 4, 3, 1 }, { 3, 2, 1 }, { 4, 1, 2 } };
     private int[] rolledDice = { 1, 1 };
 
+    //Roll the dice and get the rolled of it
     public int[] getRolled() {
         return rolledDice;
     };
