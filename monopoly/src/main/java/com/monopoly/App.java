8de655f82fcce1b72ee850b3a24b29208ea94416
Elvin Chu <elvincth@gmail.com>
2021-10-28 23:45:00 +0800

    fix

--- monopoly/src/main/java/com/monopoly/App.java
+++ monopoly/src/main/java/com/monopoly/App.java
@@ -9,9 +9,11 @@ public final class App {
 
     /**
      * Says hello to the world.
+     *
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Monopoly game = new Monopoly();
+        game.start();
     }
 }
