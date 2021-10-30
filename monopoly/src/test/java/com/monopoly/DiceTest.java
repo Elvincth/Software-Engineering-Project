3689a6210ecb5d98aa2403080984513c57b36e67
tonywong586 <72515867+tonywong586@users.noreply.github.com>
2021-10-31 02:41:12 +0800

    Dice test

--- monopoly/src/test/java/com/monopoly/DiceTest.java
+++ monopoly/src/test/java/com/monopoly/DiceTest.java
@@ -8,12 +8,30 @@ public class DiceTest {
     private Dice dice;
 
     @Test
-    void diceTest() {
+    void Dice1Test() {
         dice = new Dice(false);
 
         dice.roll();
 
-        assertTrue(dice.getRolled()[0] < 5 && dice.getRolled()[0] > 0, "Should be between 1 to/and 4");
-
+        assertTrue(dice.getRolled()[0] < 5 && dice.getRolled()[0] > 0, "Should be between 1 to 4");
     }
+
+    @Test
+    void Dice2Test() {
+        dice = new Dice(false);
+
+        dice.roll();
+
+        assertTrue(dice.getRolled()[1] < 5 && dice.getRolled()[1] > 0, "Should be between 1 to 4");
+    }
+
+    @Test
+    void mutiDiceTest(){
+        dice = new Dice(false);
+
+        dice.roll();
+
+        assertTrue(dice.getTotal() < 9 && dice.getTotal() > 0,  "Two dice should be between 1 to 9");
+    }
+
 }
