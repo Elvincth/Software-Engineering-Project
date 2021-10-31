8b9e162768e3f430ad45c3950e4c0d817289105b
Elvin Chu <elvincth@gmail.com>
2021-11-01 00:53:15 +0800

    fix

--- monopoly/src/test/java/com/monopoly/ChanceTest.java
+++ monopoly/src/test/java/com/monopoly/ChanceTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ChanceTest extends TestUtils{
+public class ChanceTest extends TestUtils {
     private Monopoly monopoly = new Monopoly(true);
     ChanceSquare chanceSquare = new ChanceSquare("TEST_PLAYER", 12);
     private Player player = new Player("TEST_PLAYER", "A");
@@ -12,36 +12,37 @@ public class ChanceTest extends TestUtils{
 
     // for checking the balance will random deduct within $300 and add within $200
     @Test
-    void testAddOrDeduct(){
-        String description = "Random deduct balance or ramdom add balance";
+    void testAddOrDeduct() {
+        String description = "Random deduct balance or random add balance";
         player.setBalance(testedBalance);
         chanceSquare.effectTo(player, monopoly);
         // calculating the balance in default $5000
         assertTrue(player.getBalance() >= 4700 && player.getBalance() <= 5200, description);
 
-        if(player.getBalance() >= 4700){
-            System.out.println("Balance After Deducted: " + "$" + player.getBalance());
+        if (player.getBalance() >= 4700) {
+            System.out.println("Balance After Deducted: $" + player.getBalance());
             System.out.println("Which is larger or equal to $4700");
-        }else if (player.getBalance() <= 5200){
-            System.out.println("Balance After Added: " + "$" + player.getBalance());
+        } else if (player.getBalance() <= 5200) {
+            System.out.println("Balance After Added: $" + player.getBalance());
             System.out.println("Which is smaller or equal to $5200");
         }
         passed(description);
     }
 
-        // for checking the ramdom number of deduction or addition can be divisible by 10
-        @Test
-        void testRamdomNumber(){
-            String description = "Ramdom number divisible by 10";
-            player.setBalance(testedBalance);
-            chanceSquare.effectTo(player, monopoly);
-            // calculating the balance in default $5000
-            assertTrue(chanceSquare.getRandomMoney() % 10 == 0, description);
+    // for checking the ramdom number of deduction or addition can be divisible by
+    // 10
+    @Test
+    void testRandomNumber() {
+        String description = "Random number divisible by 10";
+        player.setBalance(testedBalance);
+        chanceSquare.effectTo(player, monopoly);
+        // calculating the balance in default $5000
+        assertTrue(chanceSquare.getRandomMoney() % 10 == 0, description);
 
-            System.out.println("The random number of money is: " + "$" + chanceSquare.getRandomMoney());
-            System.out.println("Which can divisible by 10");
-            
-            passed(description);
-        }
-    
+        System.out.println("The random number of money is: $" + chanceSquare.getRandomMoney());
+        System.out.println("Which can divisible by 10");
+
+        passed(description);
+    }
+
 }
