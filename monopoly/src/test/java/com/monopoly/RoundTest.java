e7dfa06893e23d455e68b96103534e10bbf8c761
Elvin Chu <elvincth@gmail.com>
2021-11-02 00:26:15 +0800

    .

--- monopoly/src/test/java/com/monopoly/RoundTest.java
+++ monopoly/src/test/java/com/monopoly/RoundTest.java
@@ -3,7 +3,6 @@ package com.monopoly;
 public class RoundTest {
     private Monopoly monopoly = new Monopoly(true);
 
-    // Test if the game start correctly
 
     // Test if the game ended correctly after x round
 
