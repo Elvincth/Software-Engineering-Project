28671e772ccb2141fd10414ff65e06aa44568bca
mkcheung568 <57489128+mkcheung568@users.noreply.github.com>
2021-11-01 23:03:49 +0800

    Rename

---
+++ monopoly/src/test/java/com/monopoly/TaxSquareTest.java
@@ -0,0 +1,24 @@
+package com.monopoly;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TaxSquareTest extends TestUtils {
+    private Monopoly monopoly = new Monopoly(true);
+    private TaxSquare taxSquare = new TaxSquare("INCOME TAX", 3);
+    private Player player = new Player("TEST_PLAYER", "A");
+
+    @Test
+    void taxSquareTest() {
+        final int DEFAULT_BAL = 500; // Give a default balance to the user
+        int expectBalanceLeft = 450;
+
+        player.setBalance(DEFAULT_BAL);// Set player balance to $500
+
+        taxSquare.effectTo(player, monopoly);
+
+        assertTrue(player.getBalance() == expectBalanceLeft);
+    }
+
+}
