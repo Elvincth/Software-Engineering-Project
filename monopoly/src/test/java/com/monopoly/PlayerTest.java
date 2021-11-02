d90e320ad30e55b67ccd7c8803e4e99900b30bf1
Elvin Chu <elvincth@gmail.com>
2021-11-02 17:03:11 +0800

    fix go test

--- monopoly/src/test/java/com/monopoly/PlayerTest.java
+++ monopoly/src/test/java/com/monopoly/PlayerTest.java
@@ -45,7 +45,8 @@ public class PlayerTest extends TestUtils {
         passed(description);
     }
 
-    // Check whether did the player variables being rest after the player have set to lost
+    // Check whether did the player variables being rest after the player have set
+    // to lost
     // the game
     // After lost the user balance should be set to 0
     // and the player should release all the owned property (if any)
