a975c0e6a8c2432e151047863a8aaf1ef46640d5
Elvin Chu <elvincth@gmail.com>
2021-11-02 17:36:11 +0800

    gamedata

--- monopoly/src/main/java/com/monopoly/GameData.java
+++ monopoly/src/main/java/com/monopoly/GameData.java
@@ -1,6 +1,8 @@
 
 package com.monopoly;
 
+import java.util.ArrayList;
+
 import org.json.JSONObject;
 
 public class GameData {
@@ -11,6 +13,12 @@ public class GameData {
     }
 
     public void save() {
+        ArrayList<Player> players = monopoly.getPlayers();
+        for (int i = 0; i < players.size(); i++) {
+            Player player = players.get(i);
+            System.out.printf("%s %s %n", player.getToken(), player.getBalance());
+        }
+
         System.out.println("Save");
     }
 
