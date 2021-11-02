5fcf3773d5e525e40a7214204859b24a6b9a6d6f
BoGor918 <boscocheung1234@gmail.com>
2021-11-02 21:29:05 +0800

    GameDataTest

--- monopoly/src/test/java/com/monopoly/GameDataTest.java
+++ monopoly/src/test/java/com/monopoly/GameDataTest.java
@@ -13,11 +13,15 @@ public class GameDataTest extends TestUtils{
     private Monopoly monopoly = new Monopoly(true);
     GameData gameData = new GameData(monopoly);
 
+    // for checking is the game can be saved
     @Test
     void saveTest() {
 
         String description = "Save test correct";
 
+        // The save() method will throw exception when error occur
+        // if it can successfully call the save() method, it will return boolean true
+        // which mean it can save the game successfully
         assertTrue(gameData.save(), description);
 
         passed(description);
@@ -26,11 +30,14 @@ public class GameDataTest extends TestUtils{
     @Test
     void loadTest() throws IOException, ParseException{
 
-        // for generating the save file
+        // for generating the save file first then load back for getting data
         gameData.save();
 
         String description = "Load test correct";
 
+        // The load() method will throw exception when error occur
+        // it will print out some of the outdata that is get from the GameData.json file and also return true boolean value
+        // which mean it can successful get the data from the json file
         assertTrue(gameData.load(), description);
 
         passed(description);
